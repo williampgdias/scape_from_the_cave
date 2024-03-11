94dfa92c636ee43cfdf1f088d5f25f68d6c3ce48
William Dias <smdwilliam89@gmail.com>
2024-03-11 15:39:51 -0300

    feat(run.py): Create the intro of the game.

--- run.py
+++ run.py
@@ -1,4 +1,22 @@
-# 1. Build the history
+import time
+
+# 1. About the game
+print("Welcome to the SCAPE FROM THE CAVE game!")
+
+time.sleep(2)
+
+print("This is a RPG game and you have to choose options to run the game. "
+    "The option that you choose, will create a different way to play the game.")
+
+continue_exit = input("Do you wish to continue:\n Press Y or N: ").lower()
+
+
+
+if continue_exit == "y":
+    print("Uhuul!! Let's see if you can save everyone!")
+else:
+    print("Bye bye! Hope to see you later!")
+
 
 # 2. Tell the history about the game
 
