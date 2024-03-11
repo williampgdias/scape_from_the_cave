1128ec163afe33c3dcfe1b8ee80ef43b35a9f4a7
William Dias <smdwilliam89@gmail.com>
2024-03-11 15:57:37 -0300

    fix(run.py): Change the text to be more realistic.

--- run.py
+++ run.py
@@ -1,41 +1,47 @@
 import time
 
 # 1. About the game
-print("Welcome to the SCAPE FROM THE CAVE game!")
+import time
+
+print("Welcome to the captivating adventure of 'Escape From the Cave'!")
 
 time.sleep(2)
 
-print("This is a RPG game and you have to choose options to run the game. "
-    "The option that you choose, will create a different way to play the game.")
+print("Embark on an epic RPG journey where your choices shape the course of the game.\n"
+    "Each decision you make leads to a unique path, ensuring a thrilling and unpredictable experience.")
 
-continue_exit = input("Do you wish to continue:\n Press Y or N: ").lower()
+continue_exit = input("Are you ready to dive into the unknown?\n"
+                    "Press Y to continue or N to exit: ").lower()
+
 
 """
     Function to tell the history about the game
 """
 def about_history():
-    print("You decide to go on camp in the forest with your friends.\n " 
-        "When you get there, a big storm starts and you can't go back, because due "
-        "to the storm, there was a landslide on the hill and the road is closed.")
-    
-    time.sleep(3)
-    
-    print("While you sleep, the floor gives way and you fall into a cave. "
-        "Everyone is very hurt, and needs to find a way out.")
-    
-    time.sleep(3)
-    
-    print("Your friends trust you a lot and everyone decided that you are in charge, "
-        "because you have more experience in this kind of adventure.")
+    print("You embarked on a camping trip in the heart of the forest with your closest friends.\n"
+        "As the sun set, an unexpected storm unleashed its fury, triggering a landslide on the hill.\n"
+        "The road back home is now blocked by fallen debris.")
+
+    time.sleep(6)
+
+    print("During the restless night, disaster strikes when the ground beneath your campsite collapses,\n"
+        "plunging everyone into a mysterious cave. In the dim light, you realize everyone is injured\n"
+        "and finding an escape becomes the top priority.")
+
+    time.sleep(6)
+
+    print("In the face of uncertainty, your friends turn to you, placing their trust in your leadership.\n"
+        "With your seasoned experience in adventurous pursuits, you find yourself at the forefront\n"
+        "of this unexpected challenge, taking charge to lead your friends to safety.")
 
 
 # Conditional to start the game or not.
 if continue_exit == "y":
     print("Uhuul!! Let's see if you can save everyone!")
-    time.sleep(3)
+    time.sleep(5)
     about_history()
 else:
-    print("Bye bye! Hope to see you later!")
+    print("Bye bye! Hope to see you soon!")
 
 
 # 2. Tell the history about the game
