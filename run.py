72a5f849d2012122f0f52231c87bf16a79571603
William Dias <smdwilliam89@gmail.com>
2024-03-12 11:10:43 -0300

    feat(run.py): Start the part 2 of the game. If the user choose option 1 (explore dark tunnels), lead the a new path.

--- run.py
+++ run.py
@@ -12,9 +12,11 @@ continue_exit = input("Are you ready to dive into the unknown?\n"
                     "Press Y to continue or N to exit: ").lower()
 
 
-"""
+#################### PART 1 ####################
+
+'''
     Function to tell the history about the game
-"""
+'''
 def about_history():
     print("You embarked on a camping trip in the heart of the forest with your closest friends.\n"
         "As the sun set, an unexpected storm unleashed its fury, triggering a landslide on the hill.\n"
@@ -32,17 +34,39 @@ def about_history():
         "With your seasoned experience in adventurous pursuits, you find yourself at the forefront\n"
         "of this unexpected challenge, taking charge to lead your friends to safety.")
 
+'''
+    Option that the user has to choose to keep playing the game.
+'''
 def explore_dark_tunnels():
     print("You decide to delve into the dark tunnels, guided only by the faint glow of your flashlight.\n"
         "As you navigate through the winding passages, you discover ancient markings on the cave walls.\n"
         "Each turn presents new challenges and mysteries. The air becomes thick with anticipation.")
 
+'''
+
+'''
 def search_for_high_ground():
     print("Opting for the high ground, you lead your group to a steep incline within the cave.\n"
         "Climbing higher, you find a hidden chamber with a narrow opening leading to the surface.\n"
         "The ascent is perilous, but the promise of escape drives you and your friends forward.")
 
-# Conditional to start the game or not.
+
+#################### PART 2 ####################
+
+'''
+    Function if the user choose the explore_dark_tunnels
+'''
+def follow_mysterious_symbols():
+    print("Intrigued by the ancient symbols, you follow their trail deeper into the labyrinth of tunnels.\n"
+        "Each symbol seems to lead you further into the heart of the cave, hinting at a hidden secret.\n"
+        "The air grows colder as you press onward, anticipation building with each step.")
+
+'''
+    This IF is to start the game or exit.
+    If the user press Y, the game will start 
+    and the user will play to the end.
+    If the user press N, the game will stop
+'''
 if continue_exit == "y":
     print("Your journey begins...")
     time.sleep(3)
@@ -51,14 +75,24 @@ if continue_exit == "y":
     print("\nYou find yourself standing at a crossroads within the cave.\n"
         "Two paths stretch out before you. What will you choose?")
     
-    player_choose = input("Press 1 to explore the dark tunnels\n"
-                        "Press 2 to search for high ground\n"
-                        ">>>> ")
+    player_choose = input("Press 1 to Explore the Dark Tunnels\n"
+                        "Press 2 to Search for High Ground\n"
+                        ">>> ")
     
     if player_choose == "1":
         explore_dark_tunnels()
-    else:
+    elif player_choose == "2":
         search_for_high_ground()
+    else:
+        print("Invalid choice. The adventure awaits, make a valid selection.")
+    
+    if player_choose == '1':
+        print('')
+        time.sleep(2)
+        follow_mysterious_symbols()
+    elif player_choose == '2':
+        print('You chose the option 2')
+    
 elif continue_exit == "n":
     print("Thank you for considering 'Escape from the cave'. Hope to see you soon.")
 else:
